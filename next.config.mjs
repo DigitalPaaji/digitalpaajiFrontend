c8c780371ddf27509f7725d4a7289d2aaac2fef8
jonty1231 <jontythakur68@gmail.com>
2026-01-17 12:35:51 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  
-};
 
-export default nextConfig;
+    // output : "export",
+     trailingSlash: true,
+     images: {
+         unoptimized: true,
+     },
+ }
+ 
+ export default nextConfig;
