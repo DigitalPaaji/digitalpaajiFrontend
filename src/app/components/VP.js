0337fe547ad5f1743cb4187df019bf58de0e148a
DigitalPaaji <hello@digitalpaaji.com>
2026-02-07 16:42:24 +0530

    gn

--- src/app/components/VP.js
+++ src/app/components/VP.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { act, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -48,41 +48,6 @@ export default function About() {
     AOS.init({ duration: 1000, once: true });
   }, []);
 
-  const tabs = [
-    {
-      id: "all",
-      label: "All",
-      icon: <PiCourtBasketballFill />,
-      desc: "Boost sales with high-converting online stores.",
-    },
-    // { id: "ecommerce", label: "Ecommerce", icon: <FaShoppingCart />, desc: "Boost sales with high-converting online stores." },
-    {
-      id: "product",
-      label: "Product",
-      icon: <FaShoppingCart />,
-      desc: "Boost sales with high-converting online stores.",
-    },
-
-    {
-      id: "shoots",
-      label: "Shoots",
-      icon: <FaPassport />,
-      desc: "Hassle-free global travel documentation.",
-    },
-    // { id: "shoots", label: "Photo Shoots", icon: <FaCamera />, desc: "Professional photography for your brand." },
-    {
-      id: "restaurants",
-      label: "Restaurants",
-      icon: <FaUtensils />,
-      desc: "Culinary experiences and menu curation.",
-    },
-    {
-      id: "others",
-      label: "Others",
-      icon: <FaHotel />,
-      desc: "Luxury stays and hospitality management.",
-    },
-  ];
 
   return (
     <main>
@@ -287,48 +252,6 @@ Video Stories We&apos;ve Crafted for Our Clients
 
 
 </section>
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 md:px-12 xl:px-24 2xl:px-52  pb-20  w-full">
-        {activeTab !== "all" &&
-          allVideos
-            .filter((item) => item.type == activeTab)
-            .map((item, index) => (
-              <div
-                key={index}
-                className="relative w-full aspect-[9/16] bg-black  overflow-hidden shadow-xl border-4 group"
-              >
-                <iframe
-                  src={getEmbedUrl(item.url)}
-                  title={`YouTube Short ${index}`}
-                  className="absolute inset-0 w-full h-full"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-
-               
-                <div className="absolute inset-0 pointer-events-none bg-gradient-to-tr from-white/10 to-transparent opacity-50 rounded-2xl"></div>
-              </div>
-            ))}
-
-        {activeTab == "all" &&
-          allVideos.map((item, index) => (
-            <div
-              key={index}
-              className="relative w-full aspect-[9/16] bg-black  overflow-hidden shadow-xl border-4 group"
-            >
-              <iframe
-                src={getEmbedUrl(item.url)}
-                title={`YouTube Short ${index}`}
-                className="absolute inset-0 w-full h-full"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-
-              <div className="absolute inset-0 pointer-events-none bg-gradient-to-tr from-white/10 to-transparent opacity-50 rounded-2xl"></div>
-            </div>
-          ))}
-      </div> */}
 
       <div className="my-16">
         <Why />
@@ -336,12 +259,7 @@ Video Stories We&apos;ve Crafted for Our Clients
       <div className="my-16">
         <Services />
       </div>
-      {/* <div className="my-16">
-        <Work />
-      </div>       */}
-      {/* <div className="my-16">
-        <Faq />
-      </div> */}
+
     </main>
   );
 }
